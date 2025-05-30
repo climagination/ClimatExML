f2926128352aa95be1251d84cfde6965398717c3
Seamus Beairsto <sbeairsto@uvic.ca>
2025-05-30 09:35:48 -0700

    update model loading to just load from .pt file (vs ckpt)

--- ClimatExML/emulator/hrstream.py
+++ ClimatExML/emulator/hrstream.py
@@ -1,14 +1,11 @@
-from .base import BaseEmulator
-from models import HRStreamGenerator
+from ClimatExML.emulator.base import BaseEmulator
 
 import torch
 
+
 class HRStreamEmulator(BaseEmulator):
     def load_model(self):
-        ckpt = torch.load(self.model_path, map_location=self.device)
-        model = HRStreamGenerator(**ckpt["hyper_parameters"])
-        model.load_state_dict(ckpt["state_dict"])
-        return model
+        return torch.jit.load(self.model_path, map_location=self.device)
 
     def generate(self, lr_input, hr_invariant):
         lr_input = lr_input.to(self.device)

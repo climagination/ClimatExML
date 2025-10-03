b600b8cdc381a0db1a9a93610f42f72fe24b6904
Seamus Beairsto <seamus@l2.nibi.sharcnet>
2025-10-03 11:14:41 -0400

    update config for use on nibi DRAC

--- env_vars.sh
+++ env_vars.sh
@@ -1,5 +1,5 @@
-export OUTPUT_COMET_ZIP=/home/username/path/to/my/output/
-export PROJECT_DIR=/home/username/projects/
-export DATA_DIR=/home/username/path/to/my/data/
-export COMET_API_KEY=ABC123
+export OUTPUT_COMET_ZIP=/home/seamus/projects/def-monahana/sbeairsto/ClimatEx/trained_models
+export PROJECT_DIR=/home/seamus/projects/def-monahana/sbeairsto/ClimatEx
+export DATA_DIR=$SLURM_TMPDIR/home/sbeairsto/data-sbeairsto/drac_benchmarking/pt_subset
+export COMET_API_KEY=9rgsfqtZqfSlCDQHs5EYaUyCv
 export MODEL_PATH=/home/username/path/to/my/model.pt

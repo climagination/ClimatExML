1330217143a488e4bb9d388299680f42e20feb97
Seamus Beairsto <sbeairsto@uvic.ca>
2026-01-09 15:13:38 -0800

    remove unessecary comments

--- ClimatExML/models.py
+++ ClimatExML/models.py
@@ -285,7 +285,7 @@ class HRStreamGenerator(nn.Module):
         out = self.LR_pre(x_coarse)  ## LR branch
         outc = self.upsampling(out)
         outf = self.HR_pre(x_fine)  ## HR branch
-        out = torch.cat((outc, outf), 1)  ##combine # was breaking on this line until batch size was changed to 4 in config.yaml
+        out = torch.cat((outc, outf), 1)  ##combine
         out = self.conv3(out)
         # This precip threshold represents the 0 point in standardized space
         # based on my calculation --- this value is the negative of the mean of

3da0a3275d559633885441ca7dc04531326ae08a
Seamus Beairsto <seamus@l1.nibi.sharcnet>
2026-02-02 18:47:16 -0500

    testing setup

--- env_vars.sh
+++ env_vars.sh
@@ -1,5 +1,5 @@
-export OUTPUT_COMET_ZIP=/home/username/path/to/my/output/
-export PROJECT_DIR=/home/username/projects/
-export DATA_DIR=/home/username/path/to/my/data/
-export COMET_API_KEY=ABC123
-export MODEL_PATH=/home/username/path/to/my/model.pt
+export OUTPUT_COMET_ZIP=/home/seamus/projects/def-monahana/sbeairsto/ClimatEx/ClimatExML
+export PROJECT_DIR=/home/seamus/projects/def-monahana/sbeairsto/ClimatEx/ClimatExML
+export DATA_DIR=$SLURM_TMPDIR/van_isle_128_pt_winds
+export COMET_API_KEY=9rgsfqtZqfSlCDQHs5EYaUyCv
+export MODEL_PATH=/home/seamus/projects/def-monahana/sbeairsto/ClimatEx/ClimatExML/trained_models
